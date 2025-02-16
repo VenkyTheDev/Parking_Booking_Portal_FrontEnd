85bf2ed112c06597e333f8b244096e3d4fc822a4
Venkata Krishna Polisetty <venkata.krishna@ongrid.in>
2025-02-16 23:13:14 +0530

    Disabled the booking button when there are no available slots

--- client/src/screens/parkings/components/parkingCard.jsx
+++ client/src/screens/parkings/components/parkingCard.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Card, CardContent, CardMedia, Typography, Button, Tooltip, IconButton, Box } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Button,
+  Tooltip,
+  IconButton,
+  Box,
+} from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { HOST } from "../../../utils/constants";
 import dayjs from "dayjs";
 
-const ParkingCard = ({ parking, endTime, userInfo, onSelect, onEdit, onNavigate }) => {
+const ParkingCard = ({
+  parking,
+  endTime,
+  userInfo,
+  onSelect,
+  onEdit,
+  onNavigate,
+}) => {
   return (
     <Card
       sx={{
@@ -22,23 +38,79 @@ const ParkingCard = ({ parking, endTime, userInfo, onSelect, onEdit, onNavigate 
       <CardMedia
         component="img"
         height="200"
-        image={parking.parkingImage ? `${HOST}/${parking.parkingImage}` : `https://picsum.photos/320/240?random=${parking.id}`}
+        image={
+          parking.parkingImage
+            ? `${HOST}/${parking.parkingImage}`
+            : `https://picsum.photos/320/240?random=${parking.id}`
+        }
         alt={parking.name}
-        sx={{ objectFit: "cover", width: "100%", height: { xs: "150px", sm: "200px", md: "250px" } }}
+        sx={{
+          objectFit: "cover",
+          width: "100%",
+          height: { xs: "150px", sm: "200px", md: "250px" },
+        }}
       />
-      <CardContent sx={{ flexGrow: 1, display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
-        <Typography variant="h5" fontWeight="bold" noWrap>{parking.name}</Typography>
-        <Typography variant="h7" fontWeight="semi-bold" color="text.secondary" noWrap>
+      <CardContent
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography variant="h5" fontWeight="bold" noWrap>
+          {parking.name}
+        </Typography>
+        <Typography
+          variant="h7"
+          fontWeight="semi-bold"
+          color="text.secondary"
+          noWrap
+        >
           Total Capacity: {parking.highestSlots}
         </Typography>
-        <Typography variant="h7" fontWeight="semi-bold" color="text.secondary" noWrap>
-          Available Slots till {dayjs(endTime).format("HH:mm")}Hr: {parking.availableSlots}
+        <Typography
+          variant="h7"
+          fontWeight="semi-bold"
+          color="text.secondary"
+          noWrap
+        >
+          Available Slots till {dayjs(endTime).format("HH:mm")}Hr:{" "}
+          {parking.availableSlots}
         </Typography>
-        <Box display="flex" justifyContent="space-between" alignItems="center" mt={2}>
-          <Button variant="contained" sx={{ flexGrow: 1, borderRadius: "20px" }} onClick={() => onSelect(parking)}>Book</Button>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          mt={2}
+        >
+          <Tooltip
+            title={
+              parking.availableSlots === 0 && userInfo.role !== "ADMIN"
+                ? "No available slots"
+                : ""
+            }
+            arrow
+          >
+            <span>
+              <Button
+                variant="contained"
+                sx={{ flexGrow: 1, borderRadius: "20px" }}
+                onClick={() => onSelect(parking)}
+                disabled={
+                  parking.availableSlots === 0 && userInfo.role !== "ADMIN"
+                }
+              >
+                Book
+              </Button>
+              </span>
+          </Tooltip>
+
           {userInfo.role === "ADMIN" && (
             <Tooltip title="Edit Parking">
-              <IconButton color="primary" onClick={() => onEdit(parking)}><EditIcon /></IconButton>
+              <IconButton color="primary" onClick={() => onEdit(parking)}>
+                <EditIcon />
+              </IconButton>
             </Tooltip>
           )}
           <Tooltip title="View Location on Google Maps">
@@ -46,7 +118,12 @@ const ParkingCard = ({ parking, endTime, userInfo, onSelect, onEdit, onNavigate 
               variant="contained"
               color="secondary"
               sx={{ ml: 1, minWidth: "40px", borderRadius: "50%" }}
-              onClick={() => onNavigate(parking.location.coordinates[1], parking.location.coordinates[0])}
+              onClick={() =>
+                onNavigate(
+                  parking.location.coordinates[1],
+                  parking.location.coordinates[0]
+                )
+              }
             >
               <LocationOnIcon />
             </Button>
